straywarrior/qless

commit c8a7202d95d3ab10009973d17975dea4eb539449

Update server.rb: sort workers by their names before displaying

lib/qless/server.rb
@@ -100,7 +100,7 @@ module Qless
       end
 
       def workers
-        client.workers.counts
+        client.workers.counts.sort! { |x,y| x['name'] <=> y['name'] }
       end
 
       def failed

commit 2963d4c13efc9a518ffe73311d30774069e3fba7

Fix bugs caused by commit c8a7202, sort workers in view

lib/qless/server.rb
@@ -100,7 +100,7 @@ module Qless
       end
 
       def workers
-        client.workers.counts.sort! { |x,y| x['name'] <=> y['name'] }
+        client.workers.counts
       end
 
       def failed

lib/qless/server/views/overview.erb
@@ -123,7 +123,7 @@
       </tr>
     </thead>
     <tbody>
-    <% workers.each do |worker| %>
+    <% workers.sort { |x, y| x['name'] <=> y['name'] }.each do |worker| %>
     <tr class='worker-row'>
       <td class="large-text"><a href="<%= u "/workers/#{worker['name']}" %>"><%= worker['name'] %></a></td>
       <td><%= worker['jobs'] %></td>

lib/qless/server/views/workers.erb
@@ -2,7 +2,7 @@
   <h1>Current Workers <small>And their job counts</small></h1>
 </div>
 
-<% workers.each do |worker| %>
+<% workers.sort { |x, y| x['name'] <=> y['name'] }.each do |worker| %>
 <div class="row">
   <div class="span4">
     <h3><a href="<%= u "/workers/#{worker['name']}" %>"><%= worker['name'] %></a></h3>